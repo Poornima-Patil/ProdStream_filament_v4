9bcc0cb424cbf8dc57d6e03d9bd3789c5d14212f
Poornima Patil <poornimapatil@Poornimas-MacBook-Pro.local>
2025-01-26 13:26:03 +0530

    Removed Spatie Media

--- app/Models/Bom.php
+++ app/Models/Bom.php
@@ -6,12 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
+
 
 class Bom extends Model  implements HasMedia
 {
-    use HasFactory,SoftDeletes,InteractsWithMedia;
+    use HasFactory,SoftDeletes;
 
     protected $fillable = [
         'purchase_order_id',
