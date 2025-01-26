46d67299392baf1554843e35999bfbf052b74e92
Poornima Patil <poornimapatil@Poornimas-MacBook-Pro.local>
2025-01-26 13:29:33 +0530

    Removed Spatie Media

--- app/Models/Bom.php
+++ app/Models/Bom.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 
-class Bom extends Model  implements HasMedia
+class Bom extends Model 
 {
     use HasFactory,SoftDeletes;
 
